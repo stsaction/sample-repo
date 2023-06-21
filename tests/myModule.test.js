4c9dc25bc2e165788604ef753cc1d0ca935b87c9
stsaction <136581402+stsaction@users.noreply.github.com>
2023-06-21 16:02:45 +0530

    Update myModule.test.js

--- tests/myModule.test.js
+++ tests/myModule.test.js
@@ -2,7 +2,7 @@ const myModule = require('../src/mymodule');
 
 describe('myModule', () => {
   test('should add two numbers correctly', () => {
-    const result = myModule.add(2, 4);
+    const result = myModule.add(2, 5);
     expect(result).toBe(5);
   });
 });

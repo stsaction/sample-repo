15357e76a3f147a927c417d72b44972cdf889fe4
stsaction <136581402+stsaction@users.noreply.github.com>
2023-06-21 16:07:25 +0530

    Success commit

--- tests/myModule.test.js
+++ tests/myModule.test.js
@@ -2,7 +2,7 @@ const myModule = require('../src/mymodule');
 
 describe('myModule', () => {
   test('should add two numbers correctly', () => {
-    const result = myModule.add(2, 3);
-    expect(result).toBe(5);
+    const result = myModule.add(2, 4);
+    expect(result).toBe(6);
   });
 });

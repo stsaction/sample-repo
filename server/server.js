bdf7a1c84961ca65c57a913c41bf33b547884582
stsaction <136581402+stsaction@users.noreply.github.com>
2023-06-21 10:12:14 +0530

    Delete server directory

--- server/server.js
+++
@@ -1,8 +0,0 @@
-
-import { render, screen } from '@testing-library/react';
-import Home from '../pages/index';
-test('renders welcome message', () => {
-  render(<Home />);
-  const welcomeMessage = screen.getByText(/Welcome to Next.js/i);
-  expect(welcomeMessage).toBeInTheDocument();
-});
